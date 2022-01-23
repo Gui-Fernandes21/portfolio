fe12f2ef02706024dab8b6f5fec7eac594d08178
Guilherme <GuiFernandesBjj@gmail.com@users.noreply.github.com>
2022-01-23 17:59:53 +0100

    update-project

--- keyboard-project/behavior.js
+++ keyboard-project/behavior.js
@@ -2,7 +2,7 @@ const inputArea = document.querySelector("input");
 const keyboardContainer = document.querySelector(".keyboard-container");
 
 keyboardContainer.addEventListener("click", () => {
-  triggerFocus(input);
+  input.focus();
 });
 
 inputArea.addEventListener("focusin", () => {
@@ -14,19 +14,3 @@ inputArea.addEventListener("focusout", () => {
   keyboardContainer.style.transform = "translateY(56vh)";
   keyboardContainer.style.opacity = 0;
 });
-
-function triggerFocus(element) {
-  var eventType = "onfocusin" in element ? "focusin" : "focus",
-    bubbles = "onfocusin" in element,
-    event;
-
-  if ("createEvent" in document) {
-    event = document.createEvent("Event");
-    event.initEvent(eventType, bubbles, true);
-  } else if ("Event" in window) {
-    event = new Event(eventType, { bubbles: bubbles, cancelable: true });
-  }
-
-  element.focus();
-  element.dispatchEvent(event);
-}
